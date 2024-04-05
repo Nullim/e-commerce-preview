a04d80e5ed9d8b3782d060364d504a35b259d2dc
Viper292 <crazymelon@hotmail.es>
2024-04-05 17:15:50 -0300

    Adds promotions

---
+++ src/app/_components/Promotion/index.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+
+import classes from './index.module.scss'
+
+const Promotion = () => {
+  const [time, setTime] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  })
+
+  const targetDate = new Date()
+  targetDate.setDate(targetDate.getDate() + 28)
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      const currentTime = new Date()
+      const timeDifference = Math.max(Number(targetDate) - Number(currentTime), 0)
+
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+      const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60))
+      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000)
+
+      setTime({ days, hours, minutes, seconds })
+
+      if (timeDifference === 0) {
+        clearInterval(timerInterval)
+      }
+    }, 1000)
+
+    return () => {
+      clearInterval(timerInterval)
+    }
+  }, [])
+
+  return (
+    <section className={classes.promotion}>
+      <div className={classes.textBox}>
+        <h3 className={classes.title}>Deals of the Month</h3>
+        <p>
+          The ultimate shopping experience is here! Every month we offer
+          incredible deals on our most popular products. We also offer
+          coupons every week with your purchase, making this the perfect
+          time to get that laptop or phone you always wanted!
+          Don't miss out!
+        </p>
+        <ul className={classes.stats}>
+          <StatBox label="Days" value={time.days} />
+          <StatBox label="Hours" value={time.hours} />
+          <StatBox label="Minutes" value={time.minutes} />
+          <StatBox label="Seconds" value={time.seconds} />
+        </ul>
+        
+      </div>
+    </section>
+  )
+}
+
+const StatBox = ({ label, value }: { label: string, value: number }) => (
+  <li className={classes.statBox}>
+    <h5>{value}</h5>
+    <p>{label}</p>
+  </li>
+)
+
+export default Promotion
